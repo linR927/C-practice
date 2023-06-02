977ade7d8d97724b0f299b3edf3f84c526a0ce0a
linR927 <102715505+linR927@users.noreply.github.com>
2023-06-02 14:44:29 +0800

    Add files via upload

---
+++ password2.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+	int i = 0;
+	char password[20] = {0};
+	for (i = 0; i < 3; i++) {
+		printf("请输入密码：");
+		scanf("%s", password);
+		//if (password == "123456") ==不能用来判断两个字符串是否相等，应该使用一个库函数-strcmp
+		if (strcmp(password, "123456") == 0) {
+			printf("登录成功\n");
+			break;
+		} else {
+			printf("输入错误\n");
+		}
+	}
+	if (i == 3) {
+		printf("登录失败\n");
+	}
+	return 0;
+}
