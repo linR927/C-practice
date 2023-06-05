e02a03291fd66bfde5b609f7ffb4f657c8937e42
linR927 <102715505+linR927@users.noreply.github.com>
2023-06-05 14:23:30 +0800

    Add files via upload

---
+++ welcome to bit.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <windows.h>
+
+int main() {
+	char arr1[] = "welcome to bit!!!!!!";
+	char arr2[] = "####################";
+	int left = 0;
+	int right = strlen(arr1) - 1;
+	while (left <= right) {
+		arr2[left] = arr1[left];
+		arr2[right] = arr1[right];
+		printf("%s\n", arr2);
+		//ÐÝÏ¢Ò»Ãë
+		Sleep(1000);
+		system("cls");//cls-Çå¿ÕÆÁÄ»
+		left++;
+		right--;
+	}
+	printf("%s\n", arr2);
+	return 0;
+}
