f895883b0361f9ad70673f98ab6bf3c0161f79be
linR927 <102715505+linR927@users.noreply.github.com>
2023-06-06 11:44:24 +0800

    Update 100-200素数.cpp

--- 100-200素数.cpp
+++ 100-200素数.cpp
@@ -4,15 +4,16 @@
 int main() {
 	int i = 0;
 	int count = 0;
-	for (i = 101; i <= 200; i += 2 ) {
+	for (i = 101; i <= 200; i += 2 )
+	{
 		int j = 0;
-//		for (j = 2; j < i; j++) {
-		for (j = 2; j < sqrt(i); j++) {
+		for (j = 2; j < sqrt(i); j++) //sqrt开平方
+		{
 			if (i % j == 0)
 				break;
 		}
-		if (j > sqrt(i)) {
-			//if (j == i) {
+		if (j > sqrt(i))
+		{
 			count++;
 			printf("%d ", i);
 		}
